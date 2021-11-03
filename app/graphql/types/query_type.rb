61803e0d163cb167068cd59c211527fa135846ec
Muhammed Yousry <mohamedyousry250@gmail.com>
2021-11-04 01:11:35 +0200

    add item type and resolver

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,10 +8,16 @@ module Types
     # They will be entry points for queries on your schema.
 
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    # field :test_field, String, null: false,
+    #   description: "An example field added by the generator"
+    # def test_field
+    #   "Hello World!"
+    # end
+
+    field :items, [Types::ItemType], null: false,
+                                     description: 'Returns a list of items in the martian library'
+    def items
+      Item.all
     end
   end
 end
