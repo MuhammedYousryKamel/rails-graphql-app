166a6f1e8477971db6be69c20c5b63b2e33017a6
Muhammed Yousry <mohamedyousry250@gmail.com>
2021-11-04 13:51:45 +0200

    install apollo-client

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -7,7 +7,7 @@ module Types
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :full_name, String, null: false
-    
+
     def full_name 
       "#{object.first_name} #{object.last_name}"
     end
