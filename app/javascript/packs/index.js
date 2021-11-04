daedc7b400d97c6cbc96861604cf956042e6c6f0
Muhammed Yousry <mohamedyousry250@gmail.com>
2021-11-04 14:19:05 +0200

    add library component

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,1 +1,11 @@
-console.log('yousry 👻');
+import React from 'react';
+import { render } from 'react-dom';
+import Provider from '../components/Provider';
+import Library from '../components/Library';
+
+render(
+  <Provider>
+    <Library />
+  </Provider>,
+  document.querySelector('#root')
+)
